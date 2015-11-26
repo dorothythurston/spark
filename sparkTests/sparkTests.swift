84248ce2b603372e342f3e966c62893dcc0100f8
Dorothy Thurston <dorothy.thur@gmail.com>
2015-11-26 09:22:20 -0500

    Initial Commit

---
+++ sparkTests/sparkTests.swift
@@ -0,0 +1,36 @@
+//
+//  sparkTests.swift
+//  sparkTests
+//
+//  Created by Dorothy Thurston on 11/26/15.
+//  Copyright © 2015 Dorothy Thurston. All rights reserved.
+//
+
+import XCTest
+@testable import spark
+
+class sparkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
